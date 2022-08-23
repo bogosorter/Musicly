1d121c7426ab6a55f8a4dc2e062e5240b8c6b39f
rc4-studio <inboxaljezur@gmail.com>
2022-08-23 19:04:51 +0100

    Jekyll set up

--- src/renderer/Controller/controller.js
+++
@@ -1,9 +0,0 @@
-import PlaybackManager from './playbackmanager';
-import StateManager from './statemanager';
-
-export default class Controller {
-    constructor(setView, setPlayback, setLoading, setTheme, setTutorial) {
-        this.playback = new PlaybackManager(setPlayback);
-        this.stateManager = new StateManager(setView, setLoading, setTheme, setTutorial);
-    }
-}

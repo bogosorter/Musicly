9fd9fd3f6a53841bc92a8f39bf3f8373eb480339
m7kra <inboxaljezur@gmail.com>
2022-10-21 15:29:25 +0100

    Fixed broken cover images on Windows

--- src/renderer/Components/Cover/getCover.js
+++ src/renderer/Components/Cover/getCover.js
@@ -6,6 +6,11 @@ import emptyCover from '../../../../assets/empty.png';
 export default function getCover(album) {
     // If the current album doesn't have a defined cover, use an empty one as
     // background image
-    if (album.coverPath) return `url('file://${album.coverPath}')`;
-    else return `url('${emptyCover}')`;
+    if (album.coverPath) {
+        let path = `url('file://${album.coverPath}')`;
+        // Ensure that the path is escaped: this is needed for Windows paths.
+        path = path.replace(/\\/g, '/');
+        return path;
+    }
+    return `url('${emptyCover}')`;
 }
